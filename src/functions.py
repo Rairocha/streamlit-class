a74e05b7b22b8419b966ef7d9be036f4f4aee5ba
Rairocha <52248306+Rairocha@users.noreply.github.com>
2024-01-12 19:38:19 -0300

    trying again

--- src/functions.py
+++ src/functions.py
@@ -5,7 +5,7 @@ import os
 
 
 def load_data():
-    data = pd.read_csv('supermarket_sales.csv')
+    data = pd.read_csv('data/supermarket_sales.csv')
     data['Date'] = pd.to_datetime(data['Date'])
     return data
 
